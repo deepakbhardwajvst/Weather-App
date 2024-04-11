4a0e2cff5cd5e863d59b199c11e16a4306377b14
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-04-11 19:05:48 +0530

    feat: Create Cities page component - Created Cities page component to display a list of cities with population > 1000 - Integrated Navbar component to provide navigation within the application - Integrated CitiesTable component to display the list of cities in a table format

---
+++ src/pages/Cities.jsx
@@ -0,0 +1,11 @@
+import CitiesTable from "../components/CitiesTable";
+import Navbar from "../components/Navbar";
+
+export default function Cities() {
+  return (
+    <div>
+      <Navbar />
+      <CitiesTable />
+    </div>
+  );
+}
